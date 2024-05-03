e07423fc79844121e1eccd267343c42c5917cefe
Syedabbas110 <141505041+Syedabbas110@users.noreply.github.com>
2024-05-03 19:49:20 +0530

    Add files via upload

---
+++ frameword/app/views/about-culture.view.php
@@ -0,0 +1,10 @@
+<?php require('partials/head.php'); ?>
+
+		<header>
+			
+			<h1>About Culture</h1>
+
+		</header>
+		
+		
+<?php require('partials/footer.php'); ?>
---
+++ frameword/app/views/partials/nav.php
@@ -0,0 +1,8 @@
+<nav>
+	<ul>
+		<li><a href="index/about">About</a> </li>
+		<li><a href="index/contact">Contact </a></li>
+		<li><a href="index/about/culture">About Culture </a></li>
+		<li><a href="index/user"> Users </a></li>
+	</ul>
+</nav>
